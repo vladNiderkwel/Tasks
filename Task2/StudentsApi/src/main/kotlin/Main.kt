3233a2b82d4b3f050a182452d0fa692f5b041900
vladNiderkwel <niderkwelll@gmail.com>
2025-01-12 14:24:15 +0400

    Исправления Задания 2

--- Task2/StudentsApi/src/main/kotlin/Main.kt
+++ Task2/StudentsApi/src/main/kotlin/Main.kt
@@ -8,33 +8,36 @@ import kotlinx.serialization.json.Json
 fun main() {
     println("Server is running on http://localhost:4567")
 
-    after(Filter { _, response ->
-        with(response) {
-            header("Access-Control-Allow-Origin", "*")
-            header("Access-Control-Allow-Methods", "GET,POST,DELETE")
-            header("Content-Type", "application/json")
-            status(200)
-        }
-    })
-
     options("/*") { _, resp ->
         resp.status(200)
     }
 
     // Получение всех студентов
-    get("/student") { _, _ ->
+    get("/student") { _, resp ->
+        resp.status(200)
         Json.encodeToString(StudentService.all())
     }
 
     // Удаление студента по id
-    delete("/student/:id") { req, _ ->
+    delete("/student/:id") { req, resp ->
         val id = req.params("id").toInt()
+        resp.status(200)
         Json.encodeToString(StudentService.delete(id))
     }
 
     // Добавление нового студента
-    post("/student") { req, _ ->
+    post("/student") { req, resp ->
         val student = Json.decodeFromString<Student>(req.body())
+        resp.status(200)
         StudentService.create(student)
     }
+
+    after(Filter { _, response ->
+        with(response) {
+            header("Access-Control-Allow-Origin", "*")
+            header("Access-Control-Allow-Headers", "Content-Type")
+            header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+            header("Content-Type", "application/json")
+        }
+    })
 }
